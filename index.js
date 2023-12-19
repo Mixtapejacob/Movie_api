f9a30b422a25b32931c39559af68847517c87b2b
Mixtapejacob <144938167+Mixtapejacob@users.noreply.github.com>
2023-12-19 11:34:03 -0600

    Working with express

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+
+let topBooks = [
+    {
+      title: 'Harry Potter and the Sorcerer\'s Stone',
+      author: 'J.K. Rowling'
+    },
+    {
+      title: 'Lord of the Rings',
+      author: 'J.R.R. Tolkien'
+    },
+    {
+      title: 'Twilight',
+      author: 'Stephanie Meyer'
+    }
+  ];
+  
+  // GET requests
+  app.get('/', (req, res) => {
+    res.send('Welcome to my book club!');
+  });
+  
+  app.get('/documentation', (req, res) => {                  
+    res.sendFile('public/documentation.html', { root: __dirname });
+  });
+  
+  app.get('/books', (req, res) => {
+    res.json(topBooks);
+  });
+  
+  
+  // listen for requests
+  app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+  });
+
+  const bodyParser = require('body-parser'),
+  methodOverride = require('method-override');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+app.use((err, req, res, next) => {
+  // logic
+});
