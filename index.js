113dc2fe26926c49cd516af09e7bd653c814d8ea
Mixtapejacob <145080609+Mixtapejacob@users.noreply.github.com>
2024-04-24 18:39:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,23 @@ const { check, validationResult } = require('express-validator');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
+
 const cors = require('cors');
-app.use(cors());
+
+let allowedOrigins = ['http://localhost:1234', 'http://localhost:8080']
+
+const app = express();
+const cors = require('cors');
+app.use(cors({
+origin: (origin, callback) => {
+if(!origin) return callback(null, true);
+if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+let message = 'The CORS policy for this application doesn't allow access from origin ' + origin;
+return callback(new Error(message ), false);
+}
+return callback(null, true);
+}
+}));
 
 /* rest of code goes here*/
 let auth = require('./auth.js')(app); 
