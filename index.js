bfd0beadb279b5b8582a5123c0d2a1dee5d1cc60
John Akhilomen <jonbasanti@gmail.com>
2024-05-05 21:54:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,37 +10,32 @@ const Users = Models.User;
 
 mongoose.connect('mongodb+srv://jatakay1998:edpMGAcPBs6P7@cluster0.xrbeyjg.mongodb.net/movieapi', { useNewUrlParser: true, useUnifiedTopology: true });
 
-
 const express = require("express"),
   bodyParser = require("body-parser"),
   uuid = require("uuid"),
   methodOverride = require("method-override");
 
-const { check, validationResult } = require('express-validator');
-
 const app = express();
+const { check, validationResult } = require('express-validator');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
+// let allowedOrigins = ['http://localhost:1234', 'http://localhost:8080']
+
 const cors = require('cors');
+app.use(cors());
 
-let allowedOrigins = ['http://localhost:1234', 'http://localhost:8080']
-
-
-
-// app.use(cors());
-
-app.use(cors({
-origin: (origin, callback) => {
-if(!origin) return callback(null, true);
-if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-let message = "The CORS policy for this application doesn't allow access from origin " + origin;
-return callback(new Error(message ), false);
-}
-return callback(null, true);
-}
-}));
+// app.use(cors({
+// origin: (origin, callback) => {
+// if(!origin) return callback(null, true);
+// if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+// let message = 'The CORS policy for this application doesn't allow access from origin ' + origin;
+// return callback(new Error(message ), false);
+// }
+// return callback(null, true);
+// }
+// }));
 
 /* rest of code goes here*/
 let auth = require('./auth.js')(app); 
